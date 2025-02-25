6987f621e306efed96b72d59603ec07128273d56
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2025-02-25 10:07:15 -0300

    First controller and Razor pages tests

--- SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81488adad01158b14ddd3099f6c957598c7ae76e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.RazorTargetAssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81488adad01158b14ddd3099f6c957598c7ae76e")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
